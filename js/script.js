4f4ad1b0a966f936ad08515a6173f251ea6369e8
Ibrahim Mahrous <ebraheemalimahrous000@gmail.com>
2025-04-28 10:30:28 +0300

    Enhance performance (74/100), accessibility (93/100), best practices (96/100), and SEO (100/100) scores

--- js/script.js
+++ js/script.js
@@ -1,45 +1,36 @@
 const header = document.querySelector("header");
-
 window.addEventListener ("scroll", function() {
 	header.classList.toggle ("sticky", window.scrollY > 0);
 });
-
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
-
 menu.onclick = () => {
 	menu.classList.toggle('bx-x');
 	navlist.classList.toggle('active');
 };
-
-window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
-};
-
-const sr = ScrollReveal ({
-	distance: '45px',
-	duration: 2700,
-	reset: true
-})
-
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
-
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
-
+// window.onscroll = () => {
+// 	menu.classList.remove('bx-x');
+// 	navlist.classList.remove('active');
+// };
+// const sr = ScrollReveal ({
+// 	distance: '45px',
+// 	duration: 2700,
+// 	reset: true
+// })
+// sr.reveal('.home-text',{delay:350, origin:'left'})
+// sr.reveal('.home-img',{delay:350, origin:'right'})
+// sr.reveal('.about, .contact',{delay:200, origin:'bottom'})
+// .about, .education-section, .skills-section, .packages-section, .portfolio, .achievements, .testimonials, .contact, .ends
 
 // ** Typing
 const text = "Ibrahim Mahrous";
 const target = document.getElementById("typed-name");
 let index = 0;
-
 function typeWriter() {
   if (index < text.length) {
 	target.innerHTML += text.charAt(index);
 	index++;
-	setTimeout(typeWriter, 300); // typing speed
+	setTimeout(typeWriter, 300);
   }
 }
-
 window.onload = typeWriter;
